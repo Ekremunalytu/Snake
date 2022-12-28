16b41304ff434d156480b33ac749bc125a68ce64
Ekremunalytu <ekremunalw1@gmail.com>
2022-12-28 13:29:55 +0300

    commit

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 
 int main() {
 
+  int matrix_raw,matrix_column,Food_number;
+
+  printf("Enter the matrix size\n");
+  scanf("%d  % d", &matrix_raw , &matrix_column);
+
+  printf("Enter food number\n");
+  scanf("%d" , &Food_number);
+  printf("%d" , Food_number);
+  
+  int matrix_size = matrix_raw*matrix_column;
+  //if(Food_number > matrix_size){
+    //printf("Please enter a valid number!!!!!");
+  //};
+
+  int map [matrix_raw][matrix_column];
+  for (int i = 0; i < matrix_raw ; i++){
+    for (int j = 0; j < matrix_column ; j++){
+      map[i][j] = 0;
+    }
+  }
 
 
 
@@ -10,9 +33,5 @@ int main() {
 
 
 
-
-
-
-
-    return 0;
-}
+  return 0;
+};
