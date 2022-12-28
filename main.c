68c1b0837721d19c56b36985aa54aaa8cdcd7bab
Ekremunalytu <ekremunalw1@gmail.com>
2022-12-28 19:22:59 +0300

    commit

--- main.c
+++ main.c
@@ -7,27 +7,32 @@
 int main() {
 
   int matrix_raw,matrix_column,Food_number;
+  int snake = 1;
 
   printf("Enter the matrix size\n");
-  scanf("%d  % d", &matrix_raw , &matrix_column);
-
+  scanf("%d  %d", &matrix_raw , &matrix_column);
+  
   printf("Enter food number\n");
   scanf("%d" , &Food_number);
-  printf("%d" , Food_number);
   
   int matrix_size = matrix_raw*matrix_column;
-  //if(Food_number > matrix_size){
-    //printf("Please enter a valid number!!!!!");
-  //};
+  if(Food_number > matrix_size){
+    printf("Please enter a valid number!!!!!");
+  };
 
   int map [matrix_raw][matrix_column];
   for (int i = 0; i < matrix_raw ; i++){
     for (int j = 0; j < matrix_column ; j++){
-      map[i][j] = 0;
+      map[i][j] = '_';
     }
   }
-
-
+  
+ for (int i = 0; i < matrix_raw ; i++){
+  for (int j = 0; j < matrix_column ; j++){
+    printf("%c" , map[i][j]);
+  }
+  printf("\n");
+ }
 
 
 
