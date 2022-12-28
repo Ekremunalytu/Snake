9cacc57881792afc274d637026be9f52345c1b47
Ekremunalytu <ekremunalw1@gmail.com>
2022-12-28 21:04:12 +0300

    commit

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define UP "U"
+#define DOWN "D"
+#define LEFT "L"
+#define RIGHT "R"
 
 
 
 int main() {
   // temel değişkenler tanımlanıyor 
   int matrix_raw,matrix_column,Food_number;
-  int snake = 1;
+  int snake = 2;
   int in_game = 1;
 
   printf("Enter the matrix size\n");
@@ -30,7 +34,7 @@ int main() {
  
   int X_Coordinate, Y_Coordinate;
   int Snake_Size_Array[Food_number + 1];
-  for (int i = 0 ; i < Food_number ; i++){
+  for (int i = 0 ; i < Food_number  + 2 ; i++){
     X_Coordinate = rand() % matrix_raw  + 1;
     Y_Coordinate = rand() % matrix_column + 1;
     map[X_Coordinate][Y_Coordinate] = 1; 
@@ -46,6 +50,16 @@ int main() {
     }
     printf("\n");
   }
+  
+
+
+
+
+
+
+
+
+
 
 
 
